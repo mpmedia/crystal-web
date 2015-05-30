333ecde901a968018b54d254657c1aebe444b42a
Chris Tate <chris@studiotate.com>
2015-05-30 03:50:20 -0500

    updated codegen

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,51 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        "pkg": "package.json",
-        "sass": {
-            "dist": {
-                "files": [{
-                    "expand": true,
-                    "cwd": "src/sass",
-                    "src": [
-                        "**/*.scss"
-                    ],
-                    "dest": "src/public/css",
-                    "ext": ".css"
-                }],
-                "options": {
-                    "sourcemap": "none",
-                    "style": "compressed"
+        "name": "json",
+        "hash": {},
+        "data": {
+            "root": {
+                "loadNpmTasks": [
+                    "grunt-contrib-sass",
+                    "grunt-contrib-watch"
+                ],
+                "registerTask": {
+                    "default": [
+                        "sass"
+                    ]
+                },
+                "config": {
+                    "pkg": "package.json",
+                    "sass": {
+                        "dist": {
+                            "files": [{
+                                "expand": true,
+                                "cwd": "src/sass",
+                                "src": [
+                                    "**/*.scss"
+                                ],
+                                "dest": "src/public/css",
+                                "ext": ".css"
+                            }],
+                            "options": {
+                                "sourcemap": "none",
+                                "style": "compressed"
+                            }
+                        }
+                    },
+                    "watch": {
+                        "files": [
+                            "src/public/js/**/*.js",
+                            "src/sass/**/*.scss",
+                            "src/views/**/*.jade"
+                        ],
+                        "tasks": [
+                            "sass",
+                            "copy"
+                        ]
+                    }
                 }
             }
-        },
-        "watch": {
-            "files": [
-                "src/public/js/**/*.js",
-                "src/sass/**/*.scss",
-                "src/views/**/*.jade"
-            ],
-            "tasks": [
-                "sass",
-                "copy"
-            ]
         }
     });
 
